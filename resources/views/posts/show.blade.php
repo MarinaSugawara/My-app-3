eaf7e82ba73b678224dc7cd47c32b5d176c7876a
MarinaSugawara <o.towa.mari.0305.2007@gmail.com>
2023-09-21 11:58:48 +0900

    コメント一覧機能の作成

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,9 +2,7 @@
     <div class="container lg:w-3/4 md:w-4/5 w-11/12 mx-auto my-8 px-8 py-4 bg-white shadow-md">
 
         <x-flash-message :message="session('notice')" />
-        {{-- 終了タグはなし 文字列として変数を渡す --}}
         <x-validation-errors :errors="$errors" />
-
 
         <article class="mb-2">
             <h2 class="font-bold font-sans break-normal text-gray-900 pt-6 pb-1 text-3xl md:text-4xl break-words">
@@ -32,7 +30,6 @@
                 </form>
             @endcan
         </div>
-
         @auth
             <hr class="my-4">
 
@@ -41,5 +38,16 @@
                     class="bg-indigo-400 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline block">コメント登録</a>
             </div>
         @endauth
+
+        <section class="font-sans break-normal text-gray-900 ">
+            @foreach ($comments as $comment)
+                <div class="my-2">
+                    <span class="font-bold mr-3">{{ $comment->user->name }}</span>
+                    <span class="text-sm">{{ $comment->created_at }}</span>
+                    <p class="break-all">{!! nl2br(e($comment->body)) !!}</p>
+                </div>
+                <hr>
+            @endforeach
+        </section>
     </div>
 </x-app-layout>
